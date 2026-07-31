7e483c5aae965c031f66bd9d54bbc990649f5c5e
Nandhinireddy20 <nandhinireddy2035@gmail.com>
2026-07-31 15:11:37 +0530

    adv046 pom.xml updated

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-30T16:43:09+0530",
+    date = "2026-07-31T15:05:59+0530",
     comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
 )
 @Component
