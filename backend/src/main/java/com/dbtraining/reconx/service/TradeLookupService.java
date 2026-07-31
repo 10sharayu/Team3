86a7dccca3e259d1f52d29d547972ebeafb4bc7e
Nandhinireddy20 <nandhinireddy2035@gmail.com>
2026-07-31 21:07:38 +0530

    ticket adv058 changed made

--- backend/src/main/java/com/dbtraining/reconx/service/TradeLookupService.java
+++ backend/src/main/java/com/dbtraining/reconx/service/TradeLookupService.java
@@ -1,7 +1,7 @@
 package com.dbtraining.reconx.service;
 
-import com.dbtraining.reconx.model.Counterparty;
-import com.dbtraining.reconx.model.Trade;
+import com.dbtraining.reconx.repository.entity.Counterparty;
+import com.dbtraining.reconx.repository.entity.Trade;
 import com.dbtraining.reconx.repository.CounterpartyRepository;
 import com.dbtraining.reconx.repository.TradeRepository;
 
--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-31T16:59:58+0530",
-    comments = "version: 1.6.2, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
+    date = "2026-07-31T20:55:20+0530",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
 )
 @Component
 public class TradeMapperImpl implements TradeMapper {
